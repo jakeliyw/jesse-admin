edbcd0240ab2b4f60d963a528b5dc3a5fe4ceeb3
jakeliyw <1319569288@qq.com>
2020-09-29 00:26:38 +0800

    '完成登陆样式及条件'

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="login-container">
     <div class="login-form">
+<!--      logo名称-->
       <span class="logo">杰希集团管理系统</span>
-      <el-form ref="form" label-width="80px" class="form-content">
-        <el-input class="input-style" placeholder="请输入账号" v-model="username">
-          <i slot="prefix" class="el-input__icon el-icon-user-solid"></i>
-        </el-input>
-        <el-input class="input-style" placeholder="请选择密码" :type="passWord" v-model="password">
-          <i slot="prefix" class="el-input__icon el-icon-s-cooperation"></i>
-          <i slot="suffix" :class="icon" @click="showPass"></i>
-        </el-input>
+<!--      表单验证区域-->
+      <el-form ref="form" label-width="80px" class="form-content" :model="loginForm" :rules="rules">
+        <el-form-item prop="userName" label-width="0">
+          <el-input class="input-style" placeholder="请输入账号" v-model="loginForm.userName">
+            <i slot="prefix" class="el-input__icon el-icon-user-solid"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item label-width="0" prop="passWord">
+          <el-input class="input-style" placeholder="请选择密码" v-model="loginForm.passWord" autocomplete="off" :type="passWord">
+            <i slot="prefix" class="el-input__icon el-icon-s-cooperation"></i>
+            <i slot="suffix" :class="icon" @click="showPass"></i>
+          </el-input>
+        </el-form-item>
         <el-button type="primary" class="btn-login">登录</el-button>
       </el-form>
     </div>
@@ -21,11 +27,44 @@ export default {
   name: 'login',
   data () {
     return {
-      username: '',
-      password: '',
+      // 双休绑定账号和密码
+      loginForm: {
+        userName: '',
+        passWord: ''
+      },
+      // 密码黑色圈圈隐藏
+      passWord: 'password',
       // 用于更换Input中的图标
       icon: 'el-input__icon el-icon-view',
-      passWord: 'password'
+      // 限制条件
+      rules: {
+        userName: [
+          {
+            required: true,
+            message: '请输入账号',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 6,
+            message: '长度在 3 到 6 个字符',
+            trigger: 'blur'
+          }
+        ],
+        passWord: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 10,
+            message: '长度在 6 到 10 个字符',
+            trigger: 'blur'
+          }
+        ]
+      }
     }
   },
   methods: {
@@ -48,16 +87,13 @@ export default {
   min-height: 100vh;
   width: 100%;
   background-color: #2d3a4b;
-  overflow: hidden;
   display: flex;
   justify-content: center;
 
   .login-form {
-    width: 520px;
     height: 500px;
     display: flex;
     justify-content: center;
-    align-items: center;
     flex-direction: column;
   }
 
@@ -69,16 +105,10 @@ export default {
     font-weight: bold;
   }
 
-  .form-content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-
   .input-style ::v-deep .el-input__inner {
     height: 48px;
     width: 448px;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
   }
 
   .el-input__icon {
